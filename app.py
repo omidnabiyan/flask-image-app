802738f1ac76405f7665c3d886934380f10e9cf8
omidnabiyan <35174221+omidnabiyan@users.noreply.github.com>
2025-10-03 01:38:10 +0330

    Update app.py

--- app.py
+++ app.py
@@ -61,5 +61,9 @@ def update_preview():
     _, buffer = cv2.imencode(".jpg", img)
     return send_file(BytesIO(buffer.tobytes()), mimetype="image/jpeg")
 
-if __name__=="__main__":
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    debug = os.environ.get("FLASK_DEBUG", "0") == "1"
+    app.run(debug=debug, host="0.0.0.0", port=port)
+
